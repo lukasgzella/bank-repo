7dc43668f6812a3adae1388aa13cf8b2a0d25cbf
Lukasz Gzella <lukas.gzella@gmail.com>
2022-07-12 00:05:46 +0200

    Lesson 26 doesnt work spring jpa

--- src/main/java/org/kaczucha/repository/ClientSpringJpaRepository.java
+++ src/main/java/org/kaczucha/repository/ClientSpringJpaRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ClientSpringJpaRepository extends JpaRepository<Client, Long> {
 
     Client findByEmail(String email);
+
 }
